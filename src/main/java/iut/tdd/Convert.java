cbf9085d3fa9fd8913035d7497f8a147eaae460c
beaussart <nico.nouvelle.adresse@gmail.com>
2015-03-30 14:52:52 +0200

    Adding local

--- src/main/java/iut/tdd/Convert.java
+++ src/main/java/iut/tdd/Convert.java
@@ -4,26 +4,46 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Convert {
+	private static Map<String, Map<String, String>> dataConvert = new HashMap<String, Map<String, String>>();
+
 	private static Map<String, String> dataString = new HashMap<String, String>();
 
 	static {
-		dataString.put("0", "zero");
-		dataString.put("1", "un");
-		dataString.put("2", "deux");
-		dataString.put("3", "trois");
-		dataString.put("4", "quatre");
-		dataString.put("5", "cinq");
-		dataString.put("6", "six");
-		dataString.put("7", "spet");
-		dataString.put("8", "huit");
-		dataString.put("9", "neuf");
+		dataConvert.put("FR_fr", new HashMap<String, String>());
+		Map<String, String> fr = dataConvert.get("FR_fr");
+		fr.put("0", "zero");
+		fr.put("1", "un");
+		fr.put("2", "deux");
+		fr.put("3", "trois");
+		fr.put("4", "quatre");
+		fr.put("5", "cinq");
+		fr.put("6", "six");
+		fr.put("7", "sept");
+		fr.put("8", "huit");
+		fr.put("9", "neuf");
+
+		dataConvert.put("EN_en", new HashMap<String, String>());
+		Map<String, String> en = dataConvert.get("EN_en");
+		en.put("0", "zero");
+		en.put("1", "one");
+		en.put("2", "two");
+		en.put("3", "three");
+		en.put("4", "four");
+		en.put("5", "five");
+		en.put("6", "six");
+		en.put("7", "seven");
+		en.put("8", "eight");
+		en.put("9", "nine");
+
 	}
 
-	public static String num2text(String input) {
-		return (dataString.containsKey(input)) ? dataString.get(input) : null;
+	public static String num2text(String input, String local) {
+		return (dataConvert.containsKey(local)) ? (dataConvert.get(local)
+				.containsKey(input)) ? dataConvert.get(local).get(input) : null
+				: null;
 	}
 
-	public static String text2num(String input) {
+	public static String text2num(String input, String local) {
 		if (!dataString.values().contains(input)) {
 			return null;
 		}
--- src/test/java/iut/tdd/TestConvert.java
+++ src/test/java/iut/tdd/TestConvert.java
@@ -6,52 +6,62 @@ import org.junit.Test;
 public class TestConvert {
 	@Test
 	public void test_num2text_zero() {
-		Assert.assertEquals("zero", Convert.num2text("0"));
+		Assert.assertEquals("zero", Convert.num2text("0", "FR_fr"));
+		Assert.assertEquals("zero", Convert.num2text("0", "EN_en"));
 	}
 
 	@Test
 	public void test_num2text_un() {
-		Assert.assertEquals("un", Convert.num2text("1"));
+		Assert.assertEquals("un", Convert.num2text("1", "FR_fr"));
+		Assert.assertEquals("one", Convert.num2text("1", "EN_en"));
 	}
 
 	@Test
 	public void test_num2text_deux() {
-		Assert.assertEquals("deux", Convert.num2text("2"));
+		Assert.assertEquals("deux", Convert.num2text("2", "FR_fr"));
+		Assert.assertEquals("two", Convert.num2text("2", "EN_en"));
 	}
 
 	@Test
 	public void test_num2text_trois() {
-		Assert.assertEquals("trois", Convert.num2text("3"));
+		Assert.assertEquals("trois", Convert.num2text("3", "FR_fr"));
+		Assert.assertEquals("three", Convert.num2text("3", "EN_en"));
 	}
 
 	@Test
 	public void test_num2text_quatre() {
-		Assert.assertEquals("quatre", Convert.num2text("4"));
+		Assert.assertEquals("quatre", Convert.num2text("4", "FR_fr"));
+		Assert.assertEquals("four", Convert.num2text("4", "EN_en"));
 	}
 
 	@Test
 	public void test_num2text_cinq() {
-		Assert.assertEquals("cinq", Convert.num2text("5"));
+		Assert.assertEquals("cinq", Convert.num2text("5", "FR_fr"));
+		Assert.assertEquals("five", Convert.num2text("5", "EN_en"));
 	}
 
 	@Test
 	public void test_num2text_six() {
-		Assert.assertEquals("six", Convert.num2text("6"));
+		Assert.assertEquals("six", Convert.num2text("6", "FR_fr"));
+		Assert.assertEquals("six", Convert.num2text("6", "EN_en"));
 	}
 
 	@Test
 	public void test_num2text_sept() {
-		Assert.assertEquals("spet", Convert.num2text("7"));
+		Assert.assertEquals("sept", Convert.num2text("7", "FR_fr"));
+		Assert.assertEquals("seven", Convert.num2text("7", "EN_en"));
 	}
 
 	@Test
 	public void test_num2text_huit() {
-		Assert.assertEquals("huit", Convert.num2text("8"));
+		Assert.assertEquals("huit", Convert.num2text("8", "FR_fr"));
+		Assert.assertEquals("eight", Convert.num2text("8", "EN_en"));
 	}
 
 	@Test
 	public void test_num2text_neuf() {
-		Assert.assertEquals("neuf", Convert.num2text("9"));
+		Assert.assertEquals("neuf", Convert.num2text("9", "FR_fr"));
+		Assert.assertEquals("nine", Convert.num2text("9", "EN_en"));
 	}
 
 }
